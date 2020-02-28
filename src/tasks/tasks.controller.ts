c7e9be7ef712814afe6facf5e0df42656bdf2c73
sebasgona <sebastian.gon.a@gmail.com>
2020-02-28 15:44:07 -0600

    Add post mehtod to create a new Task

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Body } from '@nestjs/common';
+import { TasksService } from './tasks.service';
+import { Task } from './task.model';
 
 @Controller('tasks')
-export class TasksController {}
+export class TasksController {
+
+  constructor(private taskService: TasksService) { }
+
+  @Get()
+  getAllTasks() {
+    return this.taskService.getAllTasks()
+  }
+
+  @Post()
+  createTask(
+    @Body('title') title: string,
+    @Body('description') description: string
+  ): Task {
+    return this.taskService.createTask(title, description)
+  }
+}
